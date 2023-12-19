239d218c46eea6b8a54c72a33e75dbefaa9e2c44
nonaninona <starcraft0529@gmail.com>
2023-12-20 06:00:21 +0900

    handle_events

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import './App.css';
 import TopBar from './components/TopBar';
+import ViewmodeModal from './components/ViewmodeModal';
+import { useState } from "react"
 
 function App() {
+
+  const [viewmode, setViewmode] = useState(0);
+  const [viewmodeVisible, setViewmodeVisible] = useState(false);
+
+  var handleVisible = () => {
+    setViewmodeVisible(!viewmodeVisible);
+  }
+
+  var handleViewmode = () => {
+    if (viewmode == 0)
+      setViewmode(1);
+    else
+      setViewmode(0);
+  }
+
   return (
     <div className="App">
-      <TopBar className="topbar"></TopBar>
+      <TopBar handleClick={handleVisible} viewmode={viewmode}></TopBar>
+      <div className="app-viewmode-modal" style={{display: viewmodeVisible ? "block" : "none"}}>
+        <ViewmodeModal handleClick={handleViewmode} viewmode={viewmode}></ViewmodeModal>
+      </div>
+
     </div>
   );
 }
--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -3,20 +3,18 @@ import downarrow from "../assets/down-arrow.svg"
 import search from "../assets/search.svg"
 import kakaologo from "../assets/kakao_logo.png"
 import "./TopBar.css"
-import { useState } from "react"
 
 
-function TopBar() {
+function TopBar(props) {
 
-    const [viewmode, setViewmode] = useState(0);
     const viewmodeList = ["전체종목", "북마크"];
 
     return (
         <div className="topbar">
             <img src={logo} className="topbar-logo"></img>
-            <div className="topbar-viewmode">{viewmodeList[viewmode]}</div>
-            <img src={downarrow} className="topbar-downarrow"></img>
-            <div class="topbar-search">
+            <div className="topbar-viewmode" onClick={props.handleClick}>{viewmodeList[props.viewmode]}</div>
+            <img src={downarrow} onClick={props.handleClick} className="topbar-downarrow"></img>
+            <div className="topbar-search">
                 <img src={search} />
                 <input type="text" placeholder="검색어 입력" />
             </div>
@@ -25,5 +23,6 @@ function TopBar() {
         </div>
     )
 }
+
 
 export default TopBar;
